ac6de9815c03c562b5d9a22b2b731ca7a8e4a26e
Michaël Couffignal <michael@couffignal.fr>
2024-05-11 18:42:49 +0200

    init data for test

---
+++ Family-Code/src/components/AffichageCard.jsx
@@ -0,0 +1,11 @@
+function AffichageCard() {
+
+    return (
+        <>
+            <p>Ici c'est Affichagecard</p>
+            <p>Les types de données JavaScript</p>
+        </>
+    )
+}
+
+export default AffichageCard
---
+++ Family-Code/src/components/Card.jsx
@@ -0,0 +1,13 @@
+import AffichageCard from "./AffichageCard"
+
+function Card() {
+
+    return (
+        <>
+            <p>Ici c'est card</p>
+            <AffichageCard />
+        </>
+    )
+}
+
+export default Card
